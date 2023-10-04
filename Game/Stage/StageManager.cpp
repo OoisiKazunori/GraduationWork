c03efd6cf4fce0831f6858acffd4ed5b560f27e9
OoishiKazunori <cinc.higahi@gmail.com>
2023-10-04 21:47:26 +0900

    Stageのスケルトンコード用意

---
+++ Game/Stage/StageManager.cpp
@@ -0,0 +1,43 @@
+#include "StageManager.h"
+
+StageManager::StageManager() :m_changeSceneTriggerFlag(false)
+{
+	//最初のステージはこちらで読み込む
+}
+
+void StageManager::Init(DrawingByRasterize& arg_rasterize)
+{
+	m_nowStageNumber = 0;
+	m_nextStageNumber = 0;
+	m_changeSceneTriggerFlag = false;
+}
+
+void StageManager::Update()
+{
+	//ステージの切り替え処理
+	m_stage->Update();
+}
+
+void StageManager::Draw(DrawingByRasterize& arg_rasterize, Raytracing::BlasVector& arg_blasVec)
+{
+	//ステージの切り替え処理
+	if (m_nowStageNumber != m_nextStageNumber)
+	{
+		m_stage.reset();
+		m_stage = std::make_unique<StageModel>(arg_rasterize, "", "");
+		m_changeSceneTriggerFlag = true;
+	}
+
+	m_stage->Draw(arg_rasterize, arg_blasVec);
+}
+
+bool StageManager::ChangeSceneTrigger()
+{
+	if (m_changeSceneTriggerFlag)
+	{
+		bool tmpFlag = m_changeSceneTriggerFlag;
+		m_changeSceneTriggerFlag = false;
+		return tmpFlag;
+	}
+	return false;
+}
