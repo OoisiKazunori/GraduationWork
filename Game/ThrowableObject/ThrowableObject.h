f8e9a22ab1fab02aa630ebd7949a32eea7368311
Funakurabe <k020g1164@g.neec.ac.jp>
2023-11-25 20:13:04 +0900

    ウィンドウの端をつかんじゃうバグを修正

--- Game/ThrowableObject/ThrowableObject.h
+++ Game/ThrowableObject/ThrowableObject.h
@@ -1,30 +1,32 @@
-//#pragma once
-//#include "../KazLibrary/Math/KazMath.h"
-//#include "../KazLibrary/Render/BasicDraw.h"
-//
-//class ThrowableObject {
-//
-//private:
-//
-//	BasicDraw::BasicModelRender m_model;
-//	KazMath::Transform3D m_transform;
-//	KazMath::Vec3<float> m_throwVector;
-//	float m_gravity;
-//	float m_vel;
-//	bool m_isActive;
-//
-//	//重力加算量
-//	const float ADD_GRAVITY = 0.1f;
-//	//移動速度を下げる量
-//	const float SUB_VEL = 0.2f;
-//
-//public:
-//
-//	ThrowableObject(DrawingByRasterize& arg_rasterize);
-//	void Init();
-//	void Update(std::weak_ptr<MeshCollision> arg_meshCollision);
-//	void Draw(DrawingByRasterize& arg_rasterize, Raytracing::BlasVector& arg_blasVec);
-//
-//	bool GetIsActive() { return m_isActive; }
-//
-//};
+#pragma once
+#include "../KazLibrary/Math/KazMath.h"
+#include "../KazLibrary/Render/BasicDraw.h"
+
+class MeshCollision;
+
+class ThrowableObject {
+
+private:
+
+	BasicDraw::BasicModelRender m_model;
+	KazMath::Transform3D m_transform;
+	KazMath::Vec3<float> m_throwVector;
+	float m_gravity;
+	float m_vel;
+	bool m_isActive;
+
+	//重力加算量
+	const float ADD_GRAVITY = 0.1f;
+	//移動速度を下げる量
+	const float SUB_VEL = 0.2f;
+
+public:
+
+	ThrowableObject(DrawingByRasterize& arg_rasterize);
+	void Init();
+	void Update(std::list<std::shared_ptr<MeshCollision>> f_stageColliders);
+	void Draw(DrawingByRasterize& arg_rasterize, Raytracing::BlasVector& arg_blasVec);
+
+	bool GetIsActive() { return m_isActive; }
+
+};
