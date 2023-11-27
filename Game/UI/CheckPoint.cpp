e7c2a6291a76d90831ae0e0b84a91983fb2b392b
OoishiKazunori <cinc.higahi@gmail.com>
2023-11-27 10:21:32 +0900

    ゴール地点の表示クラス用意

---
+++ Game/UI/CheckPoint.cpp
@@ -0,0 +1,27 @@
+#include "CheckPoint.h"
+
+CheckPoint::CheckPoint(DrawingByRasterize& arg_rasterize) :
+	m_render(arg_rasterize, "")
+{
+}
+
+void CheckPoint::Init(const KazMath::Vec3<float>& arg_pointWorldPos)
+{
+	m_worldPos = arg_pointWorldPos;
+}
+
+void CheckPoint::Update()
+{
+	m_screenPos = KazMath::ConvertWorldPosToScreenPos(
+		m_worldPos,
+		CameraMgr::Instance()->GetViewMatrix(),
+		CameraMgr::Instance()->GetPerspectiveMatProjection()
+	);
+}
+
+void CheckPoint::Draw(DrawingByRasterize& arg_rasterize)
+{
+	KazMath::Transform2D transform;
+	transform.pos = m_screenPos.ConvertVec2();
+	m_render.m_tex.Draw2D(arg_rasterize, transform);
+}
