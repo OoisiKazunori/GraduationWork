31618180ba77eb087974d106fc16e7f43ee32a42
OoishiKazunori <cinc.higahi@gmail.com>
2023-11-27 10:48:20 +0900

    2D上に目標地点を描画。デバック中のみ座標表示

--- Game/UI/CheckPoint.cpp
+++ Game/UI/CheckPoint.cpp
@@ -1,7 +1,8 @@
 #include "CheckPoint.h"
+#include"Imgui/MyImgui.h"
 
 CheckPoint::CheckPoint(DrawingByRasterize& arg_rasterize) :
-	m_render(arg_rasterize, "Resource/Test/texas.png")
+	m_render(arg_rasterize, "Resource/Test/texas.png", true)
 {
 }
 
@@ -21,6 +22,16 @@ void CheckPoint::Update()
 
 void CheckPoint::Draw(DrawingByRasterize& arg_rasterize)
 {
+#ifdef _DEBUG
+	ImGui::Begin("ScreenPos");
+	ImGui::Text("X:%f,Y:%f,Z:%f", m_screenPos.x, m_screenPos.y, m_screenPos.z);
+	ImGui::End();
+#endif // _DEBUG
+
+	if (1.0f <= m_screenPos.z)
+	{
+		return;
+	}
 	KazMath::Transform2D transform;
 	transform.pos = m_screenPos.ConvertVec2();
 	m_render.m_tex.Draw2D(arg_rasterize, transform);
