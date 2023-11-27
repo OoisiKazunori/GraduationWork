53b2b74a9aff9fec70096521bbb71400f5e52e92
LE2A_04_OGAWA <k020g1408@g.neec.ac.jp>
2023-11-27 14:39:41 +0900

    GameScene

--- Game/Scene/GameScene.cpp
+++ Game/Scene/GameScene.cpp
@@ -242,9 +242,9 @@ void GameScene::Draw(DrawingByRasterize& arg_rasterize, Raytracing::BlasVector& 
 	m_player->Draw(arg_rasterize, arg_blasVec);
 	m_enemyManager->Draw(arg_rasterize, arg_blasVec);
 	m_line.m_render.Draw(arg_rasterize, arg_blasVec, { 0.0f,0.0f,0.0f }, { 100.0f,100.0f,100.0f }, KazMath::Color(255, 0, 0, 255));
-	m_stage.m_model.Draw(arg_rasterize, arg_blasVec, m_stageTransform);
+	//m_stage.m_model.Draw(arg_rasterize, arg_blasVec, m_stageTransform);
 
-	m_player->Draw(arg_rasterize, arg_blasVec);
+	//m_player->Draw(arg_rasterize, arg_blasVec);
 	//m_line.m_render.Draw(arg_rasterize, arg_blasVec, { 0.0f,0.0f,0.0f }, { 100.0f,100.0f,100.0f }, KazMath::Color(255, 0, 0, 255));
 	//m_stage.m_model.Draw(arg_rasterize, arg_blasVec, m_stageTransform);
 	m_bulletMgr->Draw(arg_rasterize, arg_blasVec);
