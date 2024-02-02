9de01b5b07846ff23513be776e1b416a0d7e4635
Funakurabe <k020g1164@g.neec.ac.jp>
2024-02-02 15:21:39 +0900

    プレイヤーの移動にタイヤ痕を付けました。

--- Game/Footprint/Footprint.cpp
+++ Game/Footprint/Footprint.cpp
@@ -16,6 +16,7 @@ void Footprint::Init()
 void Footprint::Generate(KazMath::Transform3D arg_generateTransform)
 {
 	m_transform = arg_generateTransform;
+	m_transform.scale = KazMath::Vec3<float>(2.5f, 2.5f, 2.5f);
 	m_isActive = true;
 	m_exitTimer = 0;
 }
--- Game/Footprint/Footprint.h
+++ Game/Footprint/Footprint.h
@@ -8,7 +8,7 @@ private:
 	BasicDraw::BasicModelRender m_model;
 	KazMath::Transform3D m_transform;
 	int m_exitTimer;
-	const int EXIT_TIMER = 240;
+	const int EXIT_TIMER = 90;
 	bool m_isActive;
 
 public:
