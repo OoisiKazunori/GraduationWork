1ea2f06bee3a7d049e56126693d30c30375f963a
OoishiKazunori <cinc.higahi@gmail.com>
2024-02-05 12:20:56 +0900

    xy軸で円状に方向ベクトルを向けた後でz軸を乱数で決める処理

--- Game/Effect/HitEffect/BulletHitWallEffect.cpp
+++ Game/Effect/HitEffect/BulletHitWallEffect.cpp
@@ -14,16 +14,32 @@ void BulletHitWallEffect::Load(DrawingByRasterize& arg_rasterize)
 
 void BulletHitWallEffect::Init(const KazMath::Vec3<float>& arg_pos)
 {
+	std::vector<KazMath::Vec3<float>>v;
+	int angle = 0;
+	//v.emplace_back(KazMath::Vec3<float>(cosf(KazMath::AngleToRadian(0)), KazMath::AngleToRadian(0), 1.0f));
+	//v.emplace_back(KazMath::Vec3<float>(cosf(KazMath::AngleToRadian(0)), KazMath::AngleToRadian(0), 1.0f));
+	//v.emplace_back(KazMath::Vec3<float>(cosf(KazMath::AngleToRadian(0)), KazMath::AngleToRadian(0), 1.0f));
+	//v.emplace_back(KazMath::Vec3<float>(cosf(KazMath::AngleToRadian(0)), KazMath::AngleToRadian(0), 1.0f));
+	//v.emplace_back(KazMath::Vec3<float>(cosf(KazMath::AngleToRadian(0)), KazMath::AngleToRadian(0), 1.0f));
+	//v.emplace_back(KazMath::Vec3<float>(cosf(KazMath::AngleToRadian(0)), KazMath::AngleToRadian(0), 1.0f));
+	//v.emplace_back(KazMath::Vec3<float>(cosf(KazMath::AngleToRadian(0)), KazMath::AngleToRadian(0), 1.0f));
+	//v.emplace_back(KazMath::Vec3<float>(cosf(KazMath::AngleToRadian(0)), KazMath::AngleToRadian(0), 1.0f));
+	//v.emplace_back(KazMath::Vec3<float>(cosf(KazMath::AngleToRadian(0)), KazMath::AngleToRadian(0), 1.0f));
+	//v.emplace_back(KazMath::Vec3<float>(cosf(KazMath::AngleToRadian(360)), KazMath::AngleToRadian(360), 1.0f));
+
+	for (int i = 0; i < 10; ++i)
+	{
+		v.emplace_back(KazMath::Vec3<float>(cosf(KazMath::AngleToRadian(i * (360 / 10))), sinf(KazMath::AngleToRadian(i * (360 / 10))), KazMath::Rand<float>(1.0f,0.0f)));
+	}
+
+	int i = 0;
 	for (auto& obj : m_particle)
 	{
 		obj.Finalize();
 		//デバック用に一個しか出さない
 		obj.Init(arg_pos,
-			KazMath::Vec3<float>(
-				KazMath::Rand(1.0f, 0.0f),
-				KazMath::Rand(1.0f, 0.0f),
-				KazMath::Rand(1.0f, 0.0f)
-			));
+			v[i]);
+		++i;
 	}
 }
 
