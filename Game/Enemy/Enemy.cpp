680cc89b63616311168e654613c42cf282eeb96e
OoishiKazunori <cinc.higahi@gmail.com>
2024-02-09 01:03:42 +0900

    無駄な処理を削除

--- Game/Enemy/Enemy.cpp
+++ Game/Enemy/Enemy.cpp
@@ -732,8 +732,6 @@ void Enemy::Death()
 		m_isSounds[Sounds::Turret_SE_3] = false;
 		m_sounds[Sounds::Turret_SE_1].source->Stop();
 		m_isSounds[Sounds::Turret_SE_1] = false;
-
-		m_trans.Rotation({ 0.0f,1.0f,1.0f }, KazMath::AngleToRadian(360.0f));
 	}
 }
 
